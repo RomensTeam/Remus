97462bd3f91f71bdd0517bd431514f55959e8035
Romens <romantrutnev@gmail.com>
2013-11-29 09:10:07 +0600

    Исправление работы роутера

--- r-core/core_module/router.php
+++ r-core/core_module/router.php
@@ -12,7 +12,7 @@ if(defined('ROUTER') && (ROUTER === 'DYNAMIC' || ROUTER === 'STATIC')){
         else{
             if(isset($uri)){
                 define('REDIRECT_URL',substr($uri,1));
-                foreach ($routing_rules as $key => $value){
+                foreach ($app as $key => $value){
                     if(preg_match($key,REDIRECT_URL)){
                         $router_file = DIR_APP_PAGE.$value;
                         $regexp_url = '/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i';
